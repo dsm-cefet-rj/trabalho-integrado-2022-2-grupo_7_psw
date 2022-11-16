9bcee04116f9f3e57c391e3a8257453158a30d37
houkaita <lucasadmaf@gmail.com>
2022-11-16 01:21:28 -0300

    Pequeno ajuste na tela de profile
    Fiz uma pequena melhora no visual/responsividade da página de profile

--- frontend/src/components/game.js
+++ frontend/src/components/game.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 function games({ gameImage }) {
   const imgStyle = {
-    width: 180,
-    height: 240,
+    width: 192,
+    height: 288,
   };
 
   return (
-    <div className="d-flex col-12 my-5 flex-column-reverse align-items-center flex-md-row">
+    <div className="d-flex col-12 my-5 flex-column-reverse align-items-center flex-md-row game">
       <Link
-        className="position-relative inline-block mx-md-4"
+        className="position-relative inline-block mx-md-1"
         to={`/screen/whatever`}
       >
         <img
--- frontend/src/routes/overview.jsx
+++ frontend/src/routes/overview.jsx
@@ -3,7 +3,6 @@ import Game from "../components/game";
 import { Link } from "react-router-dom";
 
 export default function Overview() {
-
   return (
     <>
       <div className="my-5">
@@ -22,28 +21,34 @@ export default function Overview() {
             <p className="text-light fs-6">Reviews</p>
           </Link>
         </div>
-        <div className="d-flex col-12 my-5 flex-column-reverse justify-content flex-md-row mx-5 mx-md-4">
-          <h3>Favorite from User</h3>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://static-cdn.jtvnw.net/ttv-boxart/490147_IGDB-272x380.jpg" />
-        </div>
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://image.api.playstation.com/vulcan/ap/rnd/202105/1412/bJYCCmymxvgviuuolfOx8srg.png" />
-        </div>
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://www.gamingdragons.com/images/game_img/persona4golden.jpg" />
-        </div>
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://store-images.s-microsoft.com/image/apps.64690.14291162525435146.863a4856-e937-4f6f-81c8-efcb52a1546f.725b97dc-d011-43fd-99bd-409a35646882" />
-        </div>
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://assets-prd.ignimgs.com/2021/12/08/portal2-1638924084230.jpg" />
-        </div>
-        <div className="col-6 col-md-4 col-lg-2 col-xl-2">
-          <Game gameImage="https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png" />
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col-10">
+            <div className="col-12 my-5 flex-column-reverse justify-content flex-md-row mx-4 mx-md-3">
+              <h3>Favorite from User</h3>
+            </div>
+            <div className="row">
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://static-cdn.jtvnw.net/ttv-boxart/490147_IGDB-272x380.jpg" />
+              </div>
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://image.api.playstation.com/vulcan/ap/rnd/202105/1412/bJYCCmymxvgviuuolfOx8srg.png" />
+              </div>
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://www.gamingdragons.com/images/game_img/persona4golden.jpg" />
+              </div>
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://store-images.s-microsoft.com/image/apps.64690.14291162525435146.863a4856-e937-4f6f-81c8-efcb52a1546f.725b97dc-d011-43fd-99bd-409a35646882" />
+              </div>
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://assets-prd.ignimgs.com/2021/12/08/portal2-1638924084230.jpg" />
+              </div>
+              <div className="col-6 col-md-4 col-lg-2 col-xl-2">
+                <Game gameImage="https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png" />
+              </div>
+            </div>
+          </div>
+          <div className="col-1"></div>
         </div>
       </div>
     </>
