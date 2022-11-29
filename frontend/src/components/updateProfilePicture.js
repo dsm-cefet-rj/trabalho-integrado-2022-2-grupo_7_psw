2cb18ac387b497f00fa4ddb1c592c4e7c0f82a64
vkskayo <88352993+vkskayo@users.noreply.github.com>
2022-11-29 01:44:04 -0300

    ajustes no estilo do form de perfil

--- frontend/src/components/updateProfilePicture.js
+++ frontend/src/components/updateProfilePicture.js
@@ -15,8 +15,11 @@ export default function UpdatePicture() {
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h5 className="modal-title text-dark" id="exampleModalLabel">
-                Give it a Drop!
+              <h5
+                className="modal-title text-dark text-center text-md-start"
+                id="exampleModalLabel"
+              >
+                Select your character !
               </h5>
               <button
                 type="button"
@@ -27,39 +30,43 @@ export default function UpdatePicture() {
             </div>
             <div className="modal-body">
               <div class="container-fluid">
-                <img
-                  alt="profile"
-                  className="hover-effect profile-picture"
-                  style={avatarDimension}
-                  src="https://avatars.dicebear.com/api/female/john.svg?background=%2314181c"
-                />
-                <img
-                  alt="profile"
-                  className="hover-effect profile-picture"
-                  style={avatarDimension}
-                  src="https://avatars.dicebear.com/api/male/jasdohn.svg?background=%2314181c"
-                />
-                <img
-                  alt="profile"
-                  className="hover-effect profile-picture"
-                  style={avatarDimension}
-                  src="https://avatars.dicebear.com/api/female/3ga8.svg?background=%2314181c"
-                />
-                <img
-                  alt="profile"
-                  className="hover-effect profile-picture"
-                  style={avatarDimension}
-                  src="https://avatars.dicebear.com/api/male/dsasds.svg?mood[]=happy&background=%2314181c"
-                />
+                <div className="d-flex flex-column align-items-center d-md-block">
+                  <img
+                    alt="profile"
+                    className="hover-effect profile-picture"
+                    style={avatarDimension}
+                    src="https://avatars.dicebear.com/api/female/john.svg?background=%2314181c"
+                  />
+                  <img
+                    alt="profile"
+                    className="hover-effect profile-picture"
+                    style={avatarDimension}
+                    src="https://avatars.dicebear.com/api/male/jasdohn.svg?background=%2314181c"
+                  />
+                  <img
+                    alt="profile"
+                    className="hover-effect profile-picture"
+                    style={avatarDimension}
+                    src="https://avatars.dicebear.com/api/female/3ga8.svg?background=%2314181c"
+                  />
+                  <img
+                    alt="profile"
+                    className="hover-effect profile-picture"
+                    style={avatarDimension}
+                    src="https://avatars.dicebear.com/api/male/dsasds.svg?mood[]=happy&background=%2314181c"
+                  />
+                </div>
               </div>
             </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
+            <button
+              type="button"
+              className="btn btn-warning w-50 mx-auto my-3 fs-5"
+            >
+              Next
+            </button>
+            <div className="modal-footer d-flex justify-content-start">
+              <button type="button" className="btn-lg btn-primary">
+                Update
               </button>
             </div>
           </div>
